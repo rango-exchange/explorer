0899b083d8c95f6d5a98007062646ff6190568f4
Nikaru <nikaru@rango.exchange>
2024-02-14 13:32:27 +0000

    fix: chart tooltip width

--- components/common/Layout/index.tsx
+++ components/common/Layout/index.tsx
@@ -6,16 +6,12 @@ import Footer from '../Footer';
 
 function Layout(props: PropsWithChildren<LayoutProps>) {
   return (
-    <div className="max-h-full">
+    <div className="max-h-full w-full">
       <Head>
         <title>{props.title}</title>
       </Head>
       <Navbar theme={'light'} />
-      <main
-        className="w-full min-h-600 md:min-h-[800px] pb-30 md:pb-50 bg-neutral-300  bg-body-mask bg-cover"
-        style={{
-          width: '-webkit-fill-available',
-        }}>
+      <main className="w-full min-h-600 md:min-h-[800px] pb-30 md:pb-50 bg-neutral-300  bg-body-mask bg-cover">
         {props.children}
       </main>
 
