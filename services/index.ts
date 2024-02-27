2d1cc27e995b69650e730c4185a0358fbf4714a2
rangojo <rangojo@rango.exchange>
2024-02-28 00:09:55 +0300

    fix: update api urls

--- services/index.ts
+++ services/index.ts
@@ -4,7 +4,7 @@ import { API_URL, SEARCH_RESULT_OFFSET } from '../constant';
 
 export const getLastSwaps = async () =>
   await fetch(
-    `${API_URL}/tx/latest?count=20&apiKey=${process.env.NEXT_PUBLIC_API_KEY}`,
+    `${API_URL}/dapp/tx/latest?count=20&apiKey=${process.env.NEXT_PUBLIC_API_KEY}&token=${process.env.NEXT_PUBLIC_SECRET_KEY}`,
   )
     .then(async (res) => await res.json())
     .then((data) => data?.transactions || data)
@@ -14,7 +14,7 @@ export const getLastSwaps = async () =>
     });
 
 export const getSummary = async () =>
-  await fetch(`${API_URL}/summary?apiKey=${process.env.NEXT_PUBLIC_API_KEY}`)
+  await fetch(`${API_URL}/dapp/summary?apiKey=${process.env.NEXT_PUBLIC_API_KEY}&token=${process.env.NEXT_PUBLIC_SECRET_KEY}`)
     .then(async (res) => await res.json())
     .then((data) => data)
     .catch((error) => {
@@ -24,7 +24,7 @@ export const getSummary = async () =>
 
 export const getSearchResult = async (query: string) =>
   await fetch(
-    `${API_URL}/tx/search?query=${query}&apiKey=${process.env.NEXT_PUBLIC_API_KEY}`,
+    `${API_URL}/dapp/tx/search?query=${query}&apiKey=${process.env.NEXT_PUBLIC_API_KEY}&token=${process.env.NEXT_PUBLIC_SECRET_KEY}`,
   )
     .then(async (res) => await res.json())
     .then((data) => data?.searchResult || data)
@@ -35,9 +35,9 @@ export const getSearchResult = async (query: string) =>
 
 export const getWalletSwaps = async (address: string, page?: number) =>
   await fetch(
-    `${API_URL}/tx/wallet?walletAddress=${address}&offset=${SEARCH_RESULT_OFFSET}&page=${
+    `${API_URL}/dapp/tx/wallet?walletAddress=${address}&offset=${SEARCH_RESULT_OFFSET}&page=${
       page || 0
-    }&apiKey=${process.env.NEXT_PUBLIC_API_KEY}`,
+    }&apiKey=${process.env.NEXT_PUBLIC_API_KEY}&token=${process.env.NEXT_PUBLIC_SECRET_KEY}`,
   )
     .then(async (res) => await res.json())
     .catch((error) => {
@@ -47,7 +47,7 @@ export const getWalletSwaps = async (address: string, page?: number) =>
 
 export const getTxDetails = async (requestId: string) =>
   await fetch(
-    `${API_URL}/tx/detail?requestId=${requestId}&apiKey=${process.env.NEXT_PUBLIC_API_KEY}`,
+    `${API_URL}/dapp/tx/detail?requestId=${requestId}&apiKey=${process.env.NEXT_PUBLIC_API_KEY}&token=${process.env.NEXT_PUBLIC_SECRET_KEY}`,
   )
     .then(async (res) => await res.json())
     .then((data) => data?.detailedTransaction || data)
