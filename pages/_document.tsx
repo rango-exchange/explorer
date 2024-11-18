41662fecaa213e14706b7c2d2110c97152bec47d
Nikaru <nikaru@rango.exchange>
2024-11-18 15:27:57 +0000

    feat: add google tag manager script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ import Document, {
   NextScript,
   DocumentContext,
 } from 'next/document';
+import Script from 'next/script';
 
 const description = 'Track all transactions on Rango Exchange';
 const socialTitle = 'Rango Exchange Explorer';
@@ -51,8 +52,25 @@ class MyDocument extends Document {
           <meta property="og:image:alt" content="Rango Exchange Explorer" />
           <meta property="og:description" content={description} />
           <meta property="og:site_name" content={APP_NAME} />
+
+          <Script id="gtm-script" strategy="afterInteractive">
+            {`    
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NWSR9NQK');
+
+            `}
+          </Script>
         </Head>
         <body className="bg-background">
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NWSR9NQK"  height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+
           <Main />
           <NextScript />
         </body>
